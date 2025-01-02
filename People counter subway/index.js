e09a8a5a3779807e0ea84e456aa8bd92c35d7ad6
Pratik Chandra Thakur <73338231+Pratikchandrathakur@users.noreply.github.com>
2025-01-02 15:00:02 +0545

    Update index.js

--- People counter subway/index.js
+++ People counter subway/index.js
@@ -1,1 +1,9 @@
-// Code will be updated soon
+// Here is the code:
+// intialize the count as 0
+let count = 0;
+// listen for clicks on the increment button
+btnClicked=document.getElementById("increment-btn");
+btnClicked.addEventListener('click',function(){
+  count=count+1;
+  document.getElementById("count-el").innerText=count;
+})
